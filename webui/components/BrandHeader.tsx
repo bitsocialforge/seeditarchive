92f503a13217876aaf0032517925f4d8392fc52b
Tommaso Casaburi <tom@bitsocial.net>
2026-08-14 13:05:07 +0200

    feat(branding): add archive icons

--- webui/components/BrandHeader.tsx
+++ webui/components/BrandHeader.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { siteBadge, siteName, siteTitle } from '@/lib/site';
 import { SearchBar } from './SearchBar';
@@ -7,7 +8,7 @@ export function BrandHeader() {
     <header className="site-header">
       <div className="container header-inner">
         <Link href="/" className="brand" aria-label={`${siteTitle} home`}>
-          <span className="brand-orb" aria-hidden />
+          <Image className="brand-mark" src="/brand/archive-mark.png" alt="" width={34} height={34} priority />
           <span className="brand-name">{siteName}</span>
           {siteBadge ? <span className="badge">{siteBadge}</span> : null}
         </Link>
